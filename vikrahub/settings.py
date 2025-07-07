a072f7ab174064576a8226be6727ba0a9d4b9e14
mathyeoyel <mathyeoyel@gmail.com>
2025-07-07 13:29:52 +0200

    Switch to PostgreSQL on Render

--- vikrahub/settings.py
+++ vikrahub/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -53,10 +54,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'vikrahub.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
