5eea4742c7aa30932af987ada4a84c9c5568e6bc
mathyeoyel <mathyeoyel@gmail.com>
2025-07-12 14:46:13 +0200

    modify the AWS S3 custom domain

--- vikrahub/settings.py
+++ vikrahub/settings.py
@@ -122,7 +122,7 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
-AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.eu-north-1.amazonaws.com"
+AWS_S3_CUSTOM_DOMAIN = f"https://vikrahub-media.s3.eu-north-1.amazonaws.com/"
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
